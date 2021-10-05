97bf0bb753ad31da834cbcf87e4c61a0fa579ac1
Krzysztof Baranski <krisbaranski@gmail.com>
2021-10-05 21:17:49 +0200

    feat: Add new animation pages

---
+++ animation/script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const hero = document.querySelector('.hero');
+const slider = document.querySelector('.slider');
+const logo = document.querySelector('#logo');
+const hamburger = document.querySelector('.hamburger');
+const headline = document.querySelector('.headline');
+
+const timeline = new TimelineMax();
+
+timeline
+  .fromTo(
+    hero,
+    1.4,
+    { height: '0%' },
+    { height: '80%', ease: Power2.easeInOut }
+  )
+  .fromTo(
+    hero,
+    1.2,
+    { width: '100%' },
+    { width: '80%', ease: Power2.easeInOut },
+    '-= .6'
+  )
+  .fromTo(
+    headline,
+    1,
+    { height: '20%' },
+    { height: '0%', ease: Power2.easeInOut },
+    '-= 2'
+  )
+  .fromTo(headline, 1, { opacity: 0 }, { opacity: 1 }, '-= 0.8');
